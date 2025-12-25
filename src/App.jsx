5c4290e579a91342f51ab99f5bf2b5e03c05246e
Michi <jagar008@gmail.com>
2025-12-25 23:46:46 +0100

    feat: display sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,67 @@
-import "./App.css";
-import Calendar from "./components/calendar/calendar";
-import Menu from "./components/Menu/Menu";
+import './App.css';
+import Calendar from './components/calendar/calendar';
+import { Sidebar, Menu, MenuItem, Submenu, Logo } from 'react-mui-sidebar';
+import AccessAlarms from '@mui/icons-material/AccessAlarms';
+import CottageOutlinedIcon from '@mui/icons-material/CottageOutlined';
+import { Link } from 'react-router-dom';
 
 function App() {
-
   return (
     <>
-      <h1 className="text-red-500">
-        Hello World
-      </h1>
-      <Calendar />
-      <Menu />
+      <div className="flex h-screen">
+        <Sidebar width={'270px'}>
+          <Logo
+            component={Link}
+            href="/"
+            img="https://adminmart.com/wp-content/uploads/2024/03/logo-admin-mart-news.png"
+          >
+            AdminMart
+          </Logo>
+          <Menu subHeading="HOME">
+            <MenuItem
+              icon={<CottageOutlinedIcon />}
+              component={Link}
+              link="/tes"
+              badge={true}
+              isSelected={true}
+            >
+              {' '}
+              {/* Set badge to boolean true */}
+              Modern
+            </MenuItem>
+            <MenuItem icon={<AccessAlarms />} component={Link} link="/test">
+              eCommerce
+            </MenuItem>
+            <MenuItem component={Link} link="/ana">
+              Analytical
+            </MenuItem>
+          </Menu>
+          <Menu subHeading="APPS">
+            <MenuItem>Chat</MenuItem>
+            <MenuItem>Calendar</MenuItem>
+          </Menu>
+          <Menu subHeading="OTHERS">
+            <Submenu title="Menu Level">
+              <MenuItem>Post</MenuItem>
+              <MenuItem>Details</MenuItem>
+              <Submenu title="Level 2">
+                <MenuItem>new</MenuItem>
+                <MenuItem>Hello</MenuItem>
+              </Submenu>
+            </Submenu>
+
+            <MenuItem>Chip</MenuItem>
+            <MenuItem
+              target="_blank"
+              component={Link}
+              link="https://google.com"
+            >
+              External Link
+            </MenuItem>
+          </Menu>
+        </Sidebar>
+        <Calendar />
+      </div>
     </>
   );
 }
--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,31 +1,28 @@
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import React from 'react'
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import React from 'react';
 
-const events = [
-  { title: 'Meeting', start: new Date() }
-]
+const events = [{ title: 'Meeting', start: new Date() }];
 
 export default function Calendar() {
   return (
-    <div>
-      <h1>Calendar Test</h1>
+    <div className="basis-full">
       <FullCalendar
-        plugins={ [dayGridPlugin] }
-        initialView='dayGridMonth'
-        weekwnds={ false }
-        events={ events }
-        eventContent={ renderEventContent }
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        weekwnds={false}
+        events={events}
+        eventContent={renderEventContent}
       />
     </div>
-  )
+  );
 }
 
-function renderEventContent (eventInfo) {
+function renderEventContent(eventInfo) {
   return (
     <>
       <b>{eventInfo.timeText}</b>
       <i>{eventInfo.event.title}</i>
     </>
-  )
+  );
 }
